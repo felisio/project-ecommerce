98c31a4f472341e0051bf8739caf7e7965f1ceba
Fabio Elisio <fabio.elisio@gmail.com>
2019-04-02 23:22:08 +0100

    add port

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # project-ecommerce
+
+push bakcend to heroku
+
+```
+git subtree push --prefix backend heroku-backend master
+
+```
+build front end
+
+```
+npm run build
+
+git subtree push --prefix frontend heroku-frontend master
+
+```
---
+++ frontend/.next/static/KIe80udr33RcVzksjBzkj/pages/me.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{305:function(e,n,t){__NEXT_REGISTER_PAGE("/me",function(){return e.exports=t(306),{page:e.exports.default}})},306:function(e,n,t){"use strict";t.r(n);var u=t(0),a=t.n(u);n.default=function(e){return a.a.createElement("div",null,a.a.createElement("p",null,"Page about Me!"))}}},[[305,1,0]]]);
