8889cf813801603e56fe7e3963ac9a7aa3cdd0e9
Fabio Elisio <fabio.elisio@gmail.com>
2019-07-10 22:36:18 +0100

    change header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -60,7 +60,7 @@ const Header = () => (
     <div className="bar">
       <Logo>
         <Link href="/">
-          <a>Sick Fits</a>
+          <a>Lacinhos da Lú</a>
         </Link>
       </Logo>
       <Nav />
